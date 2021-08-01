5b36065a54309d23b9975a4cd2201624b1a2fe62
hirotsugu.sakurai <sakura09100513@icloud.com>
2021-08-01 14:53:43 +0000

    first commit

---
+++ kadai-introduce.rb
@@ -0,0 +1,15 @@
+myouji = '櫻井'
+namae = '大嗣'
+gobi = 'です。'
+namaesyoukai = myouji + namae + gobi
+
+nennrei = 22
+gobi = '歳です'
+nennreisyoukai = nennrei.to_s + gobi
+
+jikosyoukai = namaesyoukai + nennreisyoukai
+puts jikosyoukai
+
+
+
+
