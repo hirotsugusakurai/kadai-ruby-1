3e80c9a69735f2459b2f850531ed3cbae140d94b
hirotsugu.sakurai <sakura09100513@icloud.com>
2021-08-02 13:33:22 +0000

    third commit

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,12 +1,11 @@
 myouji = '櫻井'
 namae = '大嗣'
-namaesyoukai = myouji + namae
-puts  namaesyoukai
-puts  'です。'
+puts  myouji + namae +  'です。'
 
 nennrei = 22
-puts nennrei.to_s 
-puts '歳です'
+puts nennrei.to_s + '歳です。'
+
+
 
 
 
